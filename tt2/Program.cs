986aeedfe0436dd0f71b99426fb11f82f1801ab6
Alexander <fiireex@yandex.ru>
2023-04-09 15:55:41 +0300

    done tt2

---
+++ tt2/Program.cs
@@ -0,0 +1,13 @@
+﻿int f(int a, int b)
+{
+  if (a == b)
+    return b;
+  return f(a + 1, b) + a; 
+}
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine(f(a, b));
